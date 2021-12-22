58c11badb7e8e62363557437dc8f686de5677bf8
phuongtt <ttphuong172@gmail.com>
2021-12-22 23:02:52 +0700

    Updated Database Login

--- src/main/java/vn/codegym/meetingroommanagement/model/user/Account.java
+++ src/main/java/vn/codegym/meetingroommanagement/model/user/Account.java
@@ -5,9 +5,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String accountId;
+    private String username;
+    private String password;
+    @OneToOne(mappedBy = "account")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name="roleId",referencedColumnName = "roleId")
+    private Role role;
+
+
 }
--- src/main/java/vn/codegym/meetingroommanagement/model/user/Role.java
+++ src/main/java/vn/codegym/meetingroommanagement/model/user/Role.java
@@ -5,9 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String roleId;
+    private String roleName;
+    @OneToMany(mappedBy = "role")
+    List<Account> accountList;
 }
