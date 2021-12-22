5196e14271f7d1f6b0a8194e93ccacd14a308fd8
Lomahs <thanh.bui.bao@gmail.com>
2021-12-22 22:00:18 +0700

    Updated Prototype

---
+++ src/main/java/vn/codegym/meetingroommanagement/MeetingRoomManagementBeApplication.java
@@ -0,0 +1,13 @@
+package vn.codegym.meetingroommanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomManagementBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingRoomManagementBeApplication.class, args);
+    }
+
+}
