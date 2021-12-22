0122c23a860ba6a2c0fda1f7a3c07f30d69a1f80
nguyenthinga-0206 <nga3421@gmail.com>
2021-12-22 22:43:28 +0700

    database history

--- src/main/java/vn/codegym/meetingroommanagement/model/history/RegistrationHistory.java
+++ src/main/java/vn/codegym/meetingroommanagement/model/history/RegistrationHistory.java
@@ -4,10 +4,116 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.model.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class RegistrationHistory {
+    @Id
+    private String id;
+    private LocalDate date_start;
+    private LocalDate date_end;
+    private LocalTime time_start;
+    private LocalTime time_end;
+    private String description;
+    private boolean is_cancle;
+//    @ManyToOne
+    private Room room;
+//    @ManyToOne
+    private User user;
+
+    public RegistrationHistory() {
+    }
+
+    public RegistrationHistory(String id, LocalDate date_start, LocalDate date_end, LocalTime time_start, LocalTime time_end, String description, boolean is_cancle, Room room, User user) {
+        this.id = id;
+        this.date_start = date_start;
+        this.date_end = date_end;
+        this.time_start = time_start;
+        this.time_end = time_end;
+        this.description = description;
+        this.is_cancle = is_cancle;
+        this.room = room;
+        this.user = user;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate_start() {
+        return date_start;
+    }
+
+    public void setDate_start(LocalDate date_start) {
+        this.date_start = date_start;
+    }
+
+    public LocalDate getDate_end() {
+        return date_end;
+    }
+
+    public void setDate_end(LocalDate date_end) {
+        this.date_end = date_end;
+    }
+
+    public LocalTime getTime_start() {
+        return time_start;
+    }
+
+    public void setTime_start(LocalTime time_start) {
+        this.time_start = time_start;
+    }
+
+    public LocalTime getTime_end() {
+        return time_end;
+    }
+
+    public void setTime_end(LocalTime time_end) {
+        this.time_end = time_end;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isIs_cancle() {
+        return is_cancle;
+    }
+
+    public void setIs_cancle(boolean is_cancle) {
+        this.is_cancle = is_cancle;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
