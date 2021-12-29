9d306c0c15ed3f1f03aa1ee7d2d1d3e6bc3c1e04
Uyen Nguyen <nguyenuyen552000@gmail.com>
2021-12-29 21:13:49 +0700

    update room UyenNTT v1

--- src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
@@ -1,9 +1,35 @@
 package vn.codegym.meetingroommanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.service.IRoomService;
+import vn.codegym.meetingroommanagement.service.impl.RoomService;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
+    @Qualifier("roomService")
+    @Autowired
+    IRoomService iRoomService;
+
+//    update a room meeting
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Room> updateRoom(@PathVariable("id") String id, @RequestBody Room room){
+      //find room by id in database
+        Optional<Room> currentRoom = iRoomService.getById(id);
+        if(currentRoom.get()==null){
+            return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
+        }
+        iRoomService.save(room);
+        return new ResponseEntity<Room>(room,HttpStatus.OK);
+
+
+    }
 }
--- src/main/java/vn/codegym/meetingroommanagement/service/impl/RoomService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/impl/RoomService.java
@@ -1,8 +1,40 @@
 package vn.codegym.meetingroommanagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.repository.IRoomRepository;
 import vn.codegym.meetingroommanagement.service.IRoomService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService implements IRoomService {
+    @Autowired
+    IRoomRepository iRoomRepository;
+    @Override
+    public List<Room> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Room> getById(String key) {
+        return iRoomRepository.findById(key);
+    }
+
+    @Override
+    public Room save(Room entity) {
+        return iRoomRepository.save(entity);
+    }
+
+    @Override
+    public void delete(Room entity) {
+
+    }
+
+    @Override
+    public void deleteById(String key) {
+
+    }
 }
