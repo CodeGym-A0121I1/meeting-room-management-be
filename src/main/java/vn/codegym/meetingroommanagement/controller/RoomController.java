b932752f84b67f2b3eddb874455fde58697b78ce
Uyen Nguyen <nguyenuyen552000@gmail.com>
2021-12-29 21:36:12 +0700

    detail room UyenNTT v1

--- src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
@@ -1,9 +1,33 @@
 package vn.codegym.meetingroommanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.service.IRoomService;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
+    @Qualifier("roomService")
+    @Autowired
+    IRoomService iRoomService;
+
+    // Detail a room in list
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Room> getRoomById(@PathVariable("id") String id) {
+        Optional<Room> room = iRoomService.getById(id);
+        if (room.get() == null) {
+            return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Room>(room.get(), HttpStatus.OK);
+    }
 }
