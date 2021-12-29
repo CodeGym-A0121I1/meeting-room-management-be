998a9c1b110c4e0bf5f7c4e28f7e87a6edbec2ba
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2021-12-29 20:45:12 +0700

    Updated EquipmentService

--- src/main/java/vn/codegym/meetingroommanagement/service/impl/EquipmentService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/impl/EquipmentService.java
@@ -1,10 +1,43 @@
 package vn.codegym.meetingroommanagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.codegym.meetingroommanagement.model.equipment.Equipment;
+import vn.codegym.meetingroommanagement.repository.IEquipmentRepository;
 import vn.codegym.meetingroommanagement.service.IEquipmentService;
+import vn.codegym.meetingroommanagement.service.IService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class EquipmentService implements IEquipmentService {
+public class EquipmentService implements IService<Equipment, String> {
 
+    @Autowired
+    IEquipmentRepository equipmentRepository;
 
+    @Override
+    public List<Equipment> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Equipment> getById(String key) {
+        return equipmentRepository.findById(key);
+    }
+
+    @Override
+    public Equipment save(Equipment entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(Equipment entity) {
+
+    }
+
+    @Override
+    public void deleteById(String key) {
+
+    }
 }
