0959f0fdaf58af541f85432f045854861376a3b0
doan cong danh <danhdoan19999@gmail.com>
2021-12-29 22:02:38 +0700

    update list room

--- src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
@@ -1,9 +1,25 @@
 package vn.codegym.meetingroommanagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.service.IRoomService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
+    @Autowired
+    private IRoomService roomService;
+    @GetMapping
+    public ResponseEntity<List<Room>> getAll() {
+        List<Room> rooms = roomService.getAll();
+        if (rooms.isEmpty()) {
+            return new ResponseEntity<List<Room>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Room>>(rooms, HttpStatus.OK);
+    }
 }
