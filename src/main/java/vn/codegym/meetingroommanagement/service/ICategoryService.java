63e49e79b101786519e85ef611f889cdac6e1898
Your Name <you@example.com>
2021-12-29 20:20:32 +0700

    push code

--- src/main/java/vn/codegym/meetingroommanagement/service/ICategoryService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/ICategoryService.java
@@ -1,7 +1,16 @@
 package vn.codegym.meetingroommanagement.service;
 
 import org.springframework.stereotype.Service;
+import vn.codegym.meetingroommanagement.model.equipment.Category;
 
-@Service
-public interface ICategoryService {
+import java.util.List;
+import java.util.Optional;
+
+public interface ICategoryService extends IService<Category, Integer> {
+
+    @Override
+    List<Category> getAll();
+
+    @Override
+    Optional<Category> getById(Integer key);
 }
