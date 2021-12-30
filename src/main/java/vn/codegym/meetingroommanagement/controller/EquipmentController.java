f4e9f5ce48457f38cbc6d0e400b20adaec128405
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2021-12-30 22:15:05 +0700

    Updated EquipmentController

--- src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
@@ -2,7 +2,6 @@ package vn.codegym.meetingroommanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.codegym.meetingroommanagement.model.equipment.Equipment;
 import vn.codegym.meetingroommanagement.service.impl.EquipmentService;
 
-import javax.swing.text.html.Option;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,15 +17,6 @@ import java.util.Optional;
 public class EquipmentController {
     @Autowired
     EquipmentService equipmentService;
-
-    @GetMapping("/")
-    public ResponseEntity<List<Equipment>> showList(){
-        List<Equipment> equipmentList = equipmentService.getAll();
-        if (equipmentList.isEmpty()){
-            return new ResponseEntity<List<Equipment>>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<List<Equipment>>(equipmentList, HttpStatus.OK);
-    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Equipment> getEquipment(@PathVariable("id") String id){
