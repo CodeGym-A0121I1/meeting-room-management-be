901846f8eadf75890d492ed15f1b5c92ffe9951c
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2021-12-30 22:24:54 +0700

    Updated EquipmentController

--- src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
@@ -1,30 +1,16 @@
 package vn.codegym.meetingroommanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 import vn.codegym.meetingroommanagement.model.equipment.Equipment;
 import vn.codegym.meetingroommanagement.service.impl.EquipmentService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/equipments")
 public class EquipmentController {
     @Autowired
     private EquipmentService equipmentService;
-
-    @GetMapping()
-    public ResponseEntity<List<Equipment>> listEquipments(){
-        List<Equipment> equipments = equipmentService.getAll();
-        if (equipments.isEmpty()){
-            return new ResponseEntity<List<Equipment>>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<List<Equipment>>(equipments, HttpStatus.OK);
-    }
 
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody Equipment equipment){
