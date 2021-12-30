804bb670d17c78669b0c575fa468cf8cdb5a95f4
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2021-12-30 22:48:19 +0700

    Updated EquipmentController

--- src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
@@ -1,16 +1,19 @@
 package vn.codegym.meetingroommanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.codegym.meetingroommanagement.model.equipment.Equipment;
 import vn.codegym.meetingroommanagement.service.impl.EquipmentService;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/equipments")
 public class EquipmentController {
     @Autowired
-    private EquipmentService equipmentService;
+    EquipmentService equipmentService;
 
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody Equipment equipment){
@@ -18,4 +21,10 @@ public class EquipmentController {
         return ResponseEntity.ok().body(equipment);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Equipment> getEquipment(@PathVariable("id") String id){
+        Optional<Equipment> equipmentOptional = equipmentService.getById(id);
+        return equipmentOptional.map(equipment -> new ResponseEntity<>(equipment, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
