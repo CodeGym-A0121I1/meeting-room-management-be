703b9ee7afb0d08d52bb68918accded521538cbb
dinth12 <thi884641@gmail.com>
2021-12-30 23:18:45 +0700

    cancel registrationHistory

--- src/main/java/vn/codegym/meetingroommanagement/controller/RegistrationHistoryController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RegistrationHistoryController.java
@@ -1,9 +1,33 @@
 package vn.codegym.meetingroommanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.codegym.meetingroommanagement.model.history.RegistrationHistory;
+import vn.codegym.meetingroommanagement.service.IRegistrationHistoryService;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/registration-histories")
 public class RegistrationHistoryController {
+    @Autowired
+    private IRegistrationHistoryService iRegistrationHistoryService;
+    @GetMapping(value = "/cancel/{id}")
+    public ResponseEntity<RegistrationHistory> cancelRegistrationHistory(@PathVariable("id") String id){
+
+        Optional<RegistrationHistory> optionalRegistrationHistory = iRegistrationHistoryService.getById(id);
+
+        if (!optionalRegistrationHistory.isPresent()){
+            return new ResponseEntity<RegistrationHistory>(HttpStatus.NOT_FOUND);
+        }
+
+        optionalRegistrationHistory.get().setCancel(true);
+        iRegistrationHistoryService.save(optionalRegistrationHistory.get());
+        return new ResponseEntity<RegistrationHistory>(HttpStatus.OK);
+    }
 }
--- src/main/java/vn/codegym/meetingroommanagement/service/impl/RegistrationHistoryService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/impl/RegistrationHistoryService.java
@@ -1,8 +1,40 @@
 package vn.codegym.meetingroommanagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.codegym.meetingroommanagement.model.history.RegistrationHistory;
+import vn.codegym.meetingroommanagement.repository.IRegistrationHistoryRepository;
 import vn.codegym.meetingroommanagement.service.IRegistrationHistoryService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RegistrationHistoryService implements IRegistrationHistoryService {
+    @Autowired
+    private IRegistrationHistoryRepository registrationHistoryRepository;
+    @Override
+    public List<RegistrationHistory> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<RegistrationHistory> getById(String key) {
+        return Optional.ofNullable(this.registrationHistoryRepository.findById(key).orElse(null));
+    }
+
+    @Override
+    public RegistrationHistory save(RegistrationHistory entity) {
+        return this.registrationHistoryRepository.save(entity);
+    }
+
+    @Override
+    public void delete(RegistrationHistory entity) {
+
+    }
+
+    @Override
+    public void deleteById(String key) {
+
+    }
 }
