a1b85f1d3f2a62216fccdcfd7b9a9f99c8fc8994
Lê Hoàng Long <lelong.ll32@gmail.com>
2021-12-31 17:23:54 +0700

    create-user-LongLH

--- src/main/java/vn/codegym/meetingroommanagement/controller/UserController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/UserController.java
@@ -1,9 +1,25 @@
 package vn.codegym.meetingroommanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.codegym.meetingroommanagement.model.user.User;
+import vn.codegym.meetingroommanagement.service.IUserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User newUser = userService.save(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
 }
--- src/main/java/vn/codegym/meetingroommanagement/service/impl/UserService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/impl/UserService.java
@@ -1,8 +1,42 @@
 package vn.codegym.meetingroommanagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.codegym.meetingroommanagement.model.user.User;
+import vn.codegym.meetingroommanagement.repository.IUserRepository;
 import vn.codegym.meetingroommanagement.service.IUserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements IUserService {
+
+    @Autowired
+    private IUserRepository iuserRepository;
+
+    @Override
+    public List<User> getAll() {
+        return iuserRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getById(String id) {
+        return iuserRepository.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return iuserRepository.save(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        iuserRepository.delete(user);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        iuserRepository.deleteById(id);
+    }
 }
