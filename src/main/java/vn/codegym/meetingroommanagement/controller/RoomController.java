10e83f8a6261df75cea0571721f5a06b9090fcb9
doan cong danh <danhdoan19999@gmail.com>
2021-12-31 17:20:18 +0700

    update delete room

--- src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
@@ -1,9 +1,31 @@
 package vn.codegym.meetingroommanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.service.IRoomService;
+
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
+    @Autowired
+    private IRoomService roomService;
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Room> deleteRoom(@PathVariable("id") String id) {
+        Optional<Room> room = roomService.getById(id);
+        if (!room.isPresent()) {
+            return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
+        }
+        roomService.deleteById(id);
+        return new ResponseEntity<Room>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/vn/codegym/meetingroommanagement/service/impl/RoomService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/impl/RoomService.java
@@ -1,8 +1,40 @@
 package vn.codegym.meetingroommanagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.codegym.meetingroommanagement.model.room.Room;
+import vn.codegym.meetingroommanagement.repository.IRoomRepository;
 import vn.codegym.meetingroommanagement.service.IRoomService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService implements IRoomService {
+    @Autowired
+    private IRoomRepository roomRepository;
+    @Override
+    public List<Room> getAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Room> getById(String key) {
+        return roomRepository.findById(key);
+    }
+
+    @Override
+    public Room save(Room entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(Room entity) {
+
+    }
+
+    @Override
+    public void deleteById(String key) {
+        roomRepository.deleteById(key);
+    }
 }
