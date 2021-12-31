f9125372fdc35bb2c7494bb24c859fc81104635c
Uyen Nguyen <nguyenuyen552000@gmail.com>
2021-12-31 09:29:33 +0700

    merge get all room

--- src/main/java/vn/codegym/meetingroommanagement/service/impl/RoomService.java
+++ src/main/java/vn/codegym/meetingroommanagement/service/impl/RoomService.java
@@ -1,5 +1,6 @@
 package vn.codegym.meetingroommanagement.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vn.codegym.meetingroommanagement.model.equipment.Equipment;
 import vn.codegym.meetingroommanagement.model.room.Room;
