8b295851ffd7fbd190fb2895159691703cd3f128
Lomahs <thanh.bui.bao@gmail.com>
2022-01-05 20:52:05 +0700

    Update Room Controller

--- src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RoomController.java
@@ -1,70 +1,66 @@
 package vn.codegym.meetingroommanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.codegym.meetingroommanagement.model.EStatus;
 import vn.codegym.meetingroommanagement.model.room.Room;
 import vn.codegym.meetingroommanagement.service.IRoomService;
+
 import java.util.List;
 import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
     @Autowired
     private IRoomService roomService;
+
     @GetMapping
-    public ResponseEntity<List<Room>> getAllRooms(){
-        return ResponseEntity.ok(roomService.getAll());
+    public ResponseEntity<List<Room>> getAllRoom() {
+        List<Room> rooms = roomService.getAll();
+        if (rooms.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
     }
-//    @GetMapping
-//    public ResponseEntity<List<Room>> getAll() {
-//        List<Room> rooms = roomService.getAll();
-//        if (rooms.isEmpty()) {
-//            return new ResponseEntity<List<Room>>(HttpStatus.NO_CONTENT);
-//        }
-//        return new ResponseEntity<List<Room>>(rooms, HttpStatus.OK);
-//    }
+
     @PostMapping
-    public ResponseEntity<Room> createRoom(@RequestBody Room room){
+    public ResponseEntity<Room> createRoom(@RequestBody Room room) {
         room.setStatus(EStatus.AVAILABLE);
-        return ResponseEntity.ok(roomService.save(room));
+
+        return new ResponseEntity<>(roomService.save(room), HttpStatus.CREATED);
     }
 
     // Detail a room in list
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Room> getRoomById(@PathVariable("id") String id) {
         Optional<Room> room = roomService.getById(id);
-        if (!room.isPresent()) {
-            return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<Room>(room.get(), HttpStatus.OK);
+
+        return room.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-//    update a room meeting
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Room> updateRoom(@PathVariable("id") String id, @RequestBody Room room){
-      //find room by id in database
-        Optional<Room> currentRoom = roomService.getById(id);
-        if (!currentRoom.isPresent()) {
-            return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
-        }
-        roomService.save(room);
-        return new ResponseEntity<Room>(room,HttpStatus.OK);
+    //    update a room meeting
+    @PutMapping
+    public ResponseEntity<Room> updateRoom(@RequestBody Room room) {
+        Optional<Room> roomOptional = roomService.getById(room.getId());
 
-
+        return roomOptional.map(r -> new ResponseEntity<>(r, HttpStatus.OK))
+                .orElseGet(ResponseEntity.notFound()::build);
     }
+
     // DanhDC xoá room
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Room> delete(@PathVariable("id") String id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> deleteRoom(@PathVariable("id") String id) {
         Optional<Room> room = roomService.getById(id);
-        if (!room.isPresent()) {
-            return new ResponseEntity<Room>(HttpStatus.NOT_FOUND);
-        }
-        roomService.deleteById(id);
-        return new ResponseEntity<Room>(HttpStatus.OK);
+
+        return room.map(r -> {
+            roomService.deleteById(id);
+
+            return ResponseEntity.ok("Delete Successful");
+        }).orElseGet(ResponseEntity.notFound()::build);
+
     }
 }
