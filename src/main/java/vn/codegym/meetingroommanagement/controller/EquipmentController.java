c02f2ef973672e16bdb3a2b98083165500238a49
tddh2001 <73831180+tddh2001@users.noreply.github.com>
2022-01-06 20:41:47 +0700

    updated EquipmentController

--- src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
@@ -4,18 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.codegym.meetingroommanagement.model.equipment.Equipment;
-import vn.codegym.meetingroommanagement.service.impl.EquipmentService;
+import vn.codegym.meetingroommanagement.service.IEquipmentService;
 
 @RestController
 @RequestMapping("/api/equipments")
 public class EquipmentController {
     @Autowired
-    private EquipmentService equipmentService;
+    private IEquipmentService equipmentService;
 
     @PostMapping("/create")
-    public ResponseEntity create(@RequestBody Equipment equipment){
+    public ResponseEntity<Equipment> create(@RequestBody Equipment equipment){
         equipmentService.save(equipment);
         return ResponseEntity.ok().body(equipment);
     }
-
 }
