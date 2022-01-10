4d05354035bbfc175fa475eff299b4e67f0ee83a
nguyenthinga-0206 <nga3421@gmail.com>
2022-01-10 10:36:51 +0700

    update NgaNT

--- src/main/java/vn/codegym/meetingroommanagement/controller/RegistrationHistoryController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/RegistrationHistoryController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.codegym.meetingroommanagement.model.history.RegistrationHistory;
 import vn.codegym.meetingroommanagement.service.IRegistrationHistoryService;
