3c40618e089985ceddad8c512c06ccb35b243812
Your Name <you@example.com>
2022-01-26 23:29:06 +0700

    update

--- src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
+++ src/main/java/vn/codegym/meetingroommanagement/controller/EquipmentController.java
@@ -4,8 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vn.codegym.meetingroommanagement.dto.CategoryDTO;
-import vn.codegym.meetingroommanagement.dto.EquipmentDTO;
+import vn.codegym.meetingroommanagement.dto.*;
 import vn.codegym.meetingroommanagement.model.EStatus;
 import vn.codegym.meetingroommanagement.model.equipment.Category;
 import vn.codegym.meetingroommanagement.model.equipment.Equipment;
@@ -34,9 +33,6 @@ public class EquipmentController {
         this.modelMapper = modelMapper;
     }
 
-    // TrongVT
-    // return: danh sách Category để dùng khi update hoặc create 1 Equipment
-    // test in Postman OK
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> getAllCategory(@RequestParam(value = "room") String roomID) {
 
@@ -70,10 +66,6 @@ public class EquipmentController {
         return categoryDTOList.isEmpty() ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(categoryDTOList, HttpStatus.OK);
     }
 
-    // TrongVT
-    // return : danh sách thiết bị thuộc 1 loại tài sản
-    // id: id của đối tượng Category
-    // test in Postman OK
     @GetMapping("/categories/{idCategory}")
     public ResponseEntity<List<Equipment>> getAllEquipmentByCategoryId(@PathVariable("idCategory") Integer idCategory) {
         List<Equipment> equipmentList = this.equipmentService.getAllByCategoryId(idCategory);
@@ -83,9 +75,6 @@ public class EquipmentController {
         return new ResponseEntity<>(equipmentList, HttpStatus.OK);
     }
 
-    // TrongVT
-    // Xóa 1 đối tượng Equipment bằng id
-    // test in Postman OK
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") String id) {
         Optional<Equipment> equipment = this.equipmentService.getById(id);
@@ -96,10 +85,6 @@ public class EquipmentController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    // TrongVT
-    // Cập nhật trường status của Equipment
-    // kiểm tra Equipment đã tồn tại chưa, nếu tồn tại thì không thực hiện Cập nhật và trả về NOT_FOUND (ngược lại)
-    // test in Postman OK
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable("id") String id, @RequestBody String status) {
         if (id == null) {
@@ -114,11 +99,6 @@ public class EquipmentController {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    // TrongVT
-    // return: danh sách Equipment khi tìm kiếm bằng name của Equipment
-    // name: name của đuối tượng Equipment
-    // idCategory: id của đối tượng Category
-    // test in Postman OK
     @GetMapping("/{idCategory}/{name}")
     public ResponseEntity<List<Equipment>> search(@PathVariable("idCategory") Integer idCategory, @PathVariable("name") String name) {
         if (idCategory == null) {
@@ -131,9 +111,6 @@ public class EquipmentController {
         return new ResponseEntity<>(equipmentList, HttpStatus.OK);
     }
 
-    // DatNT
-    // Thêm mới 1 Equipment
-    // test in Postman OK
     @PostMapping("")
     public ResponseEntity<?> create(@RequestBody EquipmentDTO equipmentDTO) {
         equipmentDTO.setStatus(EStatus.AVAILABLE);
@@ -142,15 +119,15 @@ public class EquipmentController {
         return ResponseEntity.ok().body(equipment);
     }
 
-    // DatNT
-    // return: đối tượng Equipment
-    // id: id của đối tượng Equipment
-    // test in Postman OK
     @GetMapping("/{id}")
-    public ResponseEntity<Equipment> getById(@PathVariable("id") String id) {
+    public ResponseEntity<EquipmentRequestDTO> getById(@PathVariable("id") String id) {
         Optional<Equipment> equipmentOptional = equipmentService.getById(id);
-        return equipmentOptional.map(equipment -> new ResponseEntity<>(equipment, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (equipmentOptional.isPresent()) {
+            EquipmentRequestDTO equipmentRequestDTO = modelMapper.map(equipmentOptional.get(), EquipmentRequestDTO.class);
+            equipmentRequestDTO.setRoomDTO(modelMapper.map(equipmentOptional.get(), RoomDTO.class));
+            return new ResponseEntity<>(equipmentRequestDTO, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping("/search")
